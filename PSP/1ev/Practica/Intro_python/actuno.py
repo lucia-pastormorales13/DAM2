32fb3ce8fd800193bb47f0875e1cd5046088b323
lucia <luciapasmoriesberenguer@gmail.com>
2026-09-22 14:20:50 +0200

    creada la carpeta de psp y empezado a ver python

---
+++ PSP/1ev/Practica/Intro_python/actuno.py
@@ -0,0 +1,5 @@
+#Escriba un programa que pida dos números y que escriba su media aritmética
+numero1 = float(input("Introduce el primer número: "))
+numero2 = float(input("Introduce el segundo número: "))
+media = (numero1 + numero2) / 2
+print(f"La media aritmética de {numero1} y {numero2} es {media}")
